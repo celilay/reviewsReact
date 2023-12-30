20a63bd41f27ec455533600ae6b0c371029ab1f9
celilay <celilay34@gmail.com>
2023-12-31 00:14:35 +0300

    koda yorum satırları eklendi

--- src/Review.js
+++ src/Review.js
@@ -1,50 +1,50 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from 'react'; // React import edildi
+import people from './data'; // data dosyasındaki bilgiler people olarak burda kullanılır hale getirildi.
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';  // react icon kütüphanesinden iconlar içe aktarıldı
 
-const Review = () => {
-  const [index,setIndex] = useState (0)
-  const {name,job,image,text} = people[index]
+const Review = () => { // yorumlar komponenti oluşturuldu. 
+  const [index,setIndex] = useState (0) //index 
+  const {name,job,image,text} = people[index] //people index den name job image text olarak içeri alınıyor
 
-  const checkNumber = (number) =>{
-    if(number > people.length -1){
+  const checkNumber = (number) =>{ //numaraları kontrol fonksiyonu
+    if(number > people.length -1){ // sayi people olarak aldığımız dizinin uzunluğundan büyük ise 0 geri döndürüyor.
       return 0
     }
-    if(number < 0 ){
+    if(number < 0 ){ //sayi sifirdan küçük ise peoplenın uzunluğundan bir eksik geri döndürülüyor. 
       return people.length -1
     }
     return number
   }
 
-  const nextPerson = () =>{
+  const nextPerson = () =>{ //ileri butonu için çalışan fonksiyon
     setIndex((index)=>{
       let newIndex = index + 1
-      return checkNumber(newIndex)
+      return checkNumber(newIndex) //bize gelen indexi checknumber fonksiyonuna callback olarak gönderip kontrol ediliyor.
     })
   }
 
-  const prevPerson = () =>{
+  const prevPerson = () =>{  // geri butonu 
     setIndex((index)=>{
       let newIndex = index - 1
-      return checkNumber(newIndex)
+      return checkNumber(newIndex) // sayiyi checknumber fonnksiyonuna gönderiyor.
     })
   }
 
 
-  const randomPerson = ()=>{
-    let randomNumber = Math.floor(Math.random() * people.length)
+  const randomPerson = ()=>{  // rastgele sayi oluşturmak için fonksiyon
+    let randomNumber = Math.floor(Math.random() * people.length)  //math kütüphanesinden rastgele sayı oluşturup aşşağı yuvarlanıyor.
    if(randomNumber === index){
-    randomNumber = index + 1
+    randomNumber = index + 1 // eğer index ile random sayı eşit ise indexe bir ekleniyor random sayıya aktarılıyor.
   }
   setIndex(checkNumber(randomNumber))
     console.log(randomNumber)
   }
 
-  return <article className='review'>
+  return <article className='review'> // html etiketleri oluşturulmuş.
     <div className='img-container'>
       <img src={image} alt={name} className='person-img'/>
       <span className='quote-icon'>
-      <FaQuoteRight />
+      <FaQuoteRight /> // icon eklenmiş
       </span>
     </div>
     
@@ -52,14 +52,14 @@ const Review = () => {
     <p className='job'>{job}</p>
     <p className='info'>{text}</p>
     <div className='button-container'>
-      <button className='prev-btn' onClick={prevPerson}>
+      <button className='prev-btn' onClick={prevPerson}> // butona fonksiyon eklenmiş
       <FaChevronLeft/>
       </button>
-      <button className='next-btn' onClick={nextPerson}>
+      <button className='next-btn' onClick={nextPerson}> // butona fonksiyon eklenmiş
       <FaChevronRight/>
       </button>
     </div>
-    <button className='random-btn' onClick={randomPerson}>
+    <button className='random-btn' onClick={randomPerson}>butona fonksiyon eklenmiş
       Suprise Me
       </button>
     </article>;
